6a02177502cb0a6b27c58bc9c6148bb512e93d86
Keturah Howard <keturah.dev@gmail.com>
2020-01-24 16:35:16 -0800

    change the conditions to measure new variables that hold the value as strings rather than just numbers

--- js/scripts.js
+++ js/scripts.js
@@ -8,45 +8,55 @@ $(document).ready(function () {
     var q6 = parseInt($("#q6").val());
 
     var score = q1 + q2 + q3 + q4 + q5 + q6
+    var placeholder = 9000000
+    score += placeholder;
 
-    if (score > 25) {
-      $(".mecha-hitler").show();
+    var scoreString = String(score);
+    babyYoda = scoreString[1]
+    yoda = scoreString[2]
+    hanSolo = scoreString[3]
+    ash = scoreString[4]
+    bran = scoreString[5]
+    hitler = scoreString[6]
+
+    if (babyYoda > yoda && babyYoda > hanSolo && babyYoda > ash && babyYoda > bran && babyYoda > hitler) {
+      $(".mecha-hitler").hide();
       $(".ash").hide();
       $(".han-solo").hide();
       $(".yoda").hide();
-      $(".baby-yoda").hide();
-      $(".king-hotwheels").hide();
-    } else if (score <= 25 && score > 20) {
-      $(".king-hotwheels").show();
-      $(".ash").hide();
-      $(".han-solo").hide();
-      $(".yoda").hide();
-      $(".baby-yoda").hide();
-      $(".mecha-hitler").hide();
-    } else if (score <= 20 && score > 15) {
-      $(".ash").show();
-      $(".king-hotwheels").hide();
-      $(".han-solo").hide();
-      $(".yoda").hide();
-      $(".baby-yoda").hide();
-      $(".mecha-hitler").hide();
-    } else if (score <= 15 && score > 10) {
-      $(".han-solo").show();
-      $(".mecha-hitler").hide();
-      $(".ash").hide();
-      $(".yoda").hide();
-      $(".baby-yoda").hide();
-      $(".king-hotwheels").hide();
-    } else if (score <= 10 && score > 5) {
-      $(".yoda").show();
-      $(".mecha-hitler").hide();
-      $(".king-hotwheels").hide();
-      $(".han-solo").hide();
-      $(".ash").hide();
-      $(".baby-yoda").hide();
-    } else if (score = 5) {
       $(".baby-yoda").show();
+      $(".king-hotwheels").hide();
+    } else if (yoda > babyYoda && yoda > hanSolo && yoda > ash && yoda > bran && yoda > hitler) {
+      $(".king-hotwheels").hide();
+      $(".ash").hide();
+      $(".han-solo").hide();
+      $(".yoda").show();
+      $(".baby-yoda").hide();
       $(".mecha-hitler").hide();
+    } else if (hanSolo > babyYoda && hanSolo > yoda && hanSolo > ash && hanSolo > bran && hanSolo > hitler) {
+      $(".ash").hide();
+      $(".king-hotwheels").hide();
+      $(".han-solo").show();
+      $(".yoda").hide();
+      $(".baby-yoda").hide();
+      $(".mecha-hitler").hide();
+    } else if (ash > babyYoda && ash > hanSolo && ash > yoda && ash > bran && ash > hitler) {
+      $(".han-solo").hide();
+      $(".mecha-hitler").hide();
+      $(".ash").show();
+      $(".yoda").hide();
+      $(".baby-yoda").hide();
+      $(".king-hotwheels").hide();
+    } else if (bran > babyYoda && bran > hanSolo && bran > ash && bran > yoda && bran > hitler) {
+      $(".yoda").hide();
+      $(".mecha-hitler").hide();
+      $(".king-hotwheels").bran();
+      $(".han-solo").hide();
+      $(".ash").hide();
+      $(".baby-yoda").hide();
+    } else if (hitler > babyYoda && hitler > hanSolo && hitler > ash && hitler > bran && hitler > yoda) {
+      $(".baby-yoda").hide();
+      $(".mecha-hitler").show();
       $(".king-hotwheels").hide();
       $(".ash").hide();
       $(".han-solo").hide();
